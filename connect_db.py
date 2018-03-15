022177a2528f55f3ebf2096c55763600c90c7812
laercioferracini <laercio_ferracini@yahoo.com.br>
2018-03-15 14:31:27 -0300

    Revert "First commit"
    This reverts commit 73cf0fe

--- connect_db.py
+++
@@ -1,8 +0,0 @@
-import sqlite3
-
-conn = sqlite3.connect('trades.db')
-cursor = conn.cursor()
-
-def conecta():
-    conexao = sqlite3.connect('trades.db')
-    return conexao
--- main.py
+++
@@ -1,7 +0,0 @@
-import rangeLucro
-import webbrowser
-print('Chamando o range')
-
-print(rangeLucro.rangeLucro())
-
-# webbrowser.open('http://localhost')
